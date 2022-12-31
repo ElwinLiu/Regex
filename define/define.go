058a5e195bd1f7066e9842bd6215599368c617d7
ElwinLiu <87802244+ElwinLiu@users.noreply.github.com>
2023-01-01 02:01:26 +0800

    Update define.go

--- define/define.go
+++ define/define.go
@@ -2,25 +2,25 @@ package define
 
 import "github.com/dgrijalva/jwt-go"
 
-const Port = "40010"
+const Port = ""******************""
 
 const Username = "root"
-const Password = "123456"
+const Password = ""******************""
 
 //const Username = "debian-sys-maint"
-//const Password = "xAftDU4Lgms6LLFJ"
+//const Password = ""******************""
 
 const Dbname = "terminal"
 const JwtSecret = "terminal-key"
 
 //const BucketPath = "https://terminal-1304032890.cos.ap-nanjing.myqcloud.com"
 //const SecretID = "AKIDwjL5btxnPnwsb5MPUL0TkodEfsrb6Zpq"
-//const SecretKey = "sWw73lFVXO6apppcEeuynOlvsc8SYRTP"
+//const SecretKey = ""******************""
 
 const BucketPath = "https://terminal-bucket-1314182456.cos.ap-nanjing.myqcloud.com"
 const DefaultAvatar = "https://terminal-1304032890.cos.ap-nanjing.myqcloud.com/terminal/default.png"
 const SecretID = "AKIDmQsEQahgXCev2va2whDYgbR6Zq590d8P"
-const SecretKey = "Ah4GG4f5JLBqW7d1KSTIvLcgXDh1AC2g"
+const SecretKey = "******************"
 
 const FollowerListPageSize = 15
 const MomentPageSize = 10
